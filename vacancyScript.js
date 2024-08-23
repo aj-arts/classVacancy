ec0330068ed62dd3c128d59e06d6a511745a321f
Ajinkya Gokule <ajinkyagokule@gmail.com>
2024-08-23 11:05:45 +0000

    updated coursedetails script

--- vacancyScript.js
+++ vacancyScript.js
@@ -1,47 +1,22 @@
-async function getCourseDetails(crn) {
-    let obj;
+srcdb = 202501;
 
-    const res = await fetch("https://classes.oregonstate.edu/api/?page=fose&route=search&crn=", {
-        body: `${encodeURIComponent(`{"other":{"srcdb":"202302"},"criteria":[{"field":"crn","value":"${crn}"}]}`)}`,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST"
-      })
-  
-    obj = await res.json()
-    return obj
-  }
-  
-async function getMoreDetails(course) {
-    let obj;
-    
-    const res = await fetch("https://classes.oregonstate.edu/api/?page=fose&route=details", {
-        body: `${encodeURIComponent(`{"group":"code:${course.results[0].code}","key":"crn:${course.results[0].crn}","srcdb":"202302","matched":"crn:${course.results[0].crn}"}`)}`,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST"
-    })
-    
-    obj = await res.json()
-    return obj
+async function getCourseDetailsFromCRN(crn) {
+    const url = "https://classes.oregonstate.edu/api/?page=fose&route=details";
+    const query = {
+        body: `${encodeURIComponent(`{
+                "key":"crn:${crn}",
+                "srcdb": ${srcdb}
+            }`)}`,
+        method: "POST",
+    };
+
+    const res = await fetch(url, query);
+
+    let obj = res.json();
+    return obj;
 }
 
-let course = getCourseDetails(38988)
-
+let course = getCourseDetailsFromCRN(10678);
 course.then((course) => {
-    let moreDetails = getMoreDetails(course)
-    moreDetails.then((moreDetails) => {
-        console.log(`
-        Course: ${moreDetails.code}
-        CRN: ${moreDetails.crn}
-        Max Enrollment: ${moreDetails.max_enroll}
-        Current Enrollment: ${moreDetails.enrollment}
-        Seats Available: ${moreDetails.ssbsect_seats_avail}
-        Waitlist Capacity: ${moreDetails.waitlist_capacity}
-        Waitlist Count: ${moreDetails.ssbsect_wait_count}
-        Waitlist Available: ${moreDetails.ssbsect_wait_avail}
-        `)
-    })
-})
+    console.log(course);
+});
